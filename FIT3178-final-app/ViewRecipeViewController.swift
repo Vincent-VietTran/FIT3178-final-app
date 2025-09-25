2b0d9f64c837bbed047c3b01c799c50b7cb44067
Viet Tran <vtra0041@student.monash.edu>
2025-09-25 10:45:34 +1000

    feat: Added initial vc for view recipe screen

---
+++ FIT3178-final-app/ViewRecipeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewRecipeViewController.swift
+//  FIT3178-final-app
+//
+//  Created by Viet Tran on 25/9/2025.
+//
+
+import UIKit
+
+class ViewRecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
